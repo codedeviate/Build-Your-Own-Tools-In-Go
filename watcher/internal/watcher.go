36824ae5cb79b10bb5560007cd5005dd8d1a5967
starweb-thomas <thomas.bjork@starweb.se>
2024-12-28 17:21:27 +0100

    Added debounce to watcher

--- watcher/internal/watcher.go
+++ watcher/internal/watcher.go
@@ -4,6 +4,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/gobwas/glob"
@@ -11,10 +12,11 @@ import (
 
 // Watcher monitors file changes and executes actions based on a configuration.
 type Watcher struct {
-	paths   []string
-	config  Config
-	logger  *Logger
-	watcher *fsnotify.Watcher
+	paths    []string
+	debounce map[string]map[string]*time.Timer
+	config   Config
+	logger   *Logger
+	watcher  *fsnotify.Watcher
 }
 
 // NewWatcher initializes a new Watcher.
@@ -35,6 +37,8 @@ func NewWatcher(paths []string, config Config, logger *Logger) (*Watcher, error)
 // Start begins monitoring the file system for changes.
 func (w *Watcher) Start() error {
 	defer w.watcher.Close()
+
+	w.debounce = make(map[string]map[string]*time.Timer)
 
 	// Add paths to the watcher
 	for _, path := range w.paths {
@@ -85,6 +89,16 @@ func (w *Watcher) handleEvent(event fsnotify.Event) {
 				if eventType != event.Op.String() {
 					continue
 				}
+				if _, ok := w.debounce[event.Name]; !ok {
+					w.debounce[event.Name] = make(map[string]*time.Timer)
+				}
+				if _, ok := w.debounce[event.Name][eventType]; !!ok {
+					w.debounce[event.Name][eventType].Reset(100 * time.Millisecond)
+					continue
+				}
+				w.debounce[event.Name][eventType] = time.AfterFunc(100*time.Millisecond, func() {
+					delete(w.debounce[event.Name], eventType)
+				})
 				w.logger.Info("%v", event.String())
 				for _, action := range actionList {
 					// w.logger.Info("Match found: %s -> %s", pattern, event.Name)
