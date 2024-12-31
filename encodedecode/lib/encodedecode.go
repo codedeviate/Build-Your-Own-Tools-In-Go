60a8d4ebd1f51459da615b3d5a7b7e3863c4842b
starweb-thomas <thomas.bjork@starweb.se>
2024-12-31 13:21:03 +0100

    Fixed formatting in file

--- encodedecode/lib/encodedecode.go
+++ encodedecode/lib/encodedecode.go
@@ -1,4 +1,4 @@
-// encoder.go
+// encodedecode.go
 package lib
 
 import (
@@ -8,28 +8,28 @@ import (
 
 // Base64Encode encodes a string to Base64.
 func Base64Encode(data string) string {
-    return base64.StdEncoding.EncodeToString([]byte(data))
+	return base64.StdEncoding.EncodeToString([]byte(data))
 }
 
 // Base64Decode decodes a Base64 string.
 func Base64Decode(data string) (string, error) {
-    decoded, err := base64.StdEncoding.DecodeString(data)
-    if err != nil {
-        return "", err
-    }
-    return string(decoded), nil
+	decoded, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return "", err
+	}
+	return string(decoded), nil
 }
 
 // URLEncode encodes a string to URL encoding.
 func URLEncode(data string) string {
-    return url.QueryEscape(data)
+	return url.QueryEscape(data)
 }
 
 // URLDecode decodes a URL encoded string.
 func URLDecode(data string) (string, error) {
-    decoded, err := url.QueryUnescape(data)
-    if err != nil {
-        return "", err
-    }
-    return decoded, nil
+	decoded, err := url.QueryUnescape(data)
+	if err != nil {
+		return "", err
+	}
+	return decoded, nil
 }
